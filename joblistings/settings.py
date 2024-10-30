69529b78cf7538b6473fda360e5a3eac960f3453
theomaro <theo.pombe@gmail.com>
2024-10-30 19:55:11 +0300

    update static root

--- joblistings/settings.py
+++ joblistings/settings.py
@@ -136,8 +136,8 @@ if DEBUG:
     STATICFILES_DIRS = [
         BASE_DIR / "static",
     ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Default primary key field type
